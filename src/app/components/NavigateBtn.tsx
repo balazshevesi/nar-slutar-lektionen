747ec019c74ded26cc5eaddd99406fb1c5f1773c
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2023-12-01 23:19:09 +0100

    Add support for "/" in name

--- src/app/components/NavigateBtn.tsx
+++ src/app/components/NavigateBtn.tsx
@@ -11,14 +11,15 @@ export default function NavigateBtn({
   routeName: string;
 }) {
   const includesASlash = namn.includes("/");
-
-  if (includesASlash) {
-    return <InvalidNavigate namn={namn} />;
-  }
+  const decodedRouteName = decodeURI(routeName);
+  const includesASlashRoute = decodedRouteName.replace(
+    namn,
+    encodeURIComponent(namn),
+  );
 
   return (
     <Link
-      href={`${routeName}`}
+      href={`${includesASlash ? includesASlashRoute : routeName}`}
       className="relative flex w-52 items-center justify-center gap-1 overflow-hidden rounded-xl border border-slate-100 bg-gradient-to-t from-slate-100 to-white py-2 text-center shadow outline-2 outline-offset-2 outline-slate-400 focus:outline [&>svg]:hover:w-5 "
     >
       <div>{namn}</div>

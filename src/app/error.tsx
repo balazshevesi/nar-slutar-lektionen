18c193da9c14fc2df161cd478c24112e76bb8f8d
Balazs <121605136+Balazs-topg@users.noreply.github.com>
2023-12-05 19:16:23 +0100

    Build error page

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Title2 from "@/components/general/Title2";
+import Section from "@/components/layout/Section";
+
+export default function Error() {
+  return (
+    <Section>
+      <Title2>Ett fel uppstod tyvärr :(</Title2>
+    </Section>
+  );
+}
